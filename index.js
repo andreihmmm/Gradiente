b0efe4eaf7534e2f1e5ef79964e968ec3cb60ab1
andreihmmm <124471216+andreihmmm@users.noreply.github.com>
2023-05-16 17:12:03 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -8,15 +8,52 @@ function funcaoKelvin() {
 
 
     var outputCelsius = document.querySelector("#r_celsius")
-    outputCelsius.textContent = Celsius
+    outputCelsius.textContent = parseInt(Celsius) + " ºC"
     var outputKelvin = document.querySelector("#r_kelvin")
-    outputKelvin.textContent = Kelvin
+    outputKelvin.textContent = parseInt(Kelvin) + " K"
     var outputFahrenheit = document.querySelector("#r_fahrenheit")
-    outputFahrenheit.textContent = Fahrenheit
+    outputFahrenheit.textContent = parseInt(Fahrenheit) + " ºF"
 
-    if (Celsius >= 100) {
-        body.style.backgroundColor = red
+    var red = ''
+    var green = ''
+    var blue = ''
+    if (Celsius <= 0) {
+        red = 0;
+        green = 0;
+        blue = 255;
+    } else if (Celsius <= 50) {
+        red = (255 * Celsius / 50);
+        green = (255 * Celsius / 50);
+        blue = 255 - (255 * Celsius / 50);
+    } else if (Celsius <= 100) {
+        red = 255;
+        green = 255 - (255 * Celsius / 100);
+        blue = 0;
+    } else {
+        red = 255;
+        green = 0;
+        blue = 0;
     }
+    redHex = parseInt(red)
+    var r = redHex.toString(16)
+    if (r.length < 2) {
+        r = "0" + r
+    }
+    greenHex = parseInt(green)
+    var g = greenHex.toString(16)
+    if (g.length < 2) {
+        g = "0" + g
+    }
+    blueHex = parseInt(blue)
+    var b = blueHex.toString(16)
+    if (b.length < 2) {
+        b = "0" + b
+    }
+
+
+    var rgb = "#" + r + g + b
+    var body = document.querySelector("body")
+    var corFundo = body.style.backgroundColor = rgb
 }
 
 function funcaoCelsius() {
@@ -26,23 +63,104 @@ function funcaoCelsius() {
     var Fahrenheit = 1.8 * Celsius + 32
 
     var outputCelsius = document.querySelector("#r_celsius")
-    outputCelsius.textContent = Celsius
+    outputCelsius.textContent = parseInt(Celsius) + " ºC"
     var outputKelvin = document.querySelector("#r_kelvin")
-    outputKelvin.textContent = Kelvin
+    outputKelvin.textContent = parseInt(Kelvin) + " K"
     var outputFahrenheit = document.querySelector("#r_fahrenheit")
-    outputFahrenheit.textContent = Fahrenheit
+    outputFahrenheit.textContent = parseInt(Fahrenheit) + " ºF"
+
+    var red = ''
+    var green = ''
+    var blue = ''
+    if (Celsius <= 0) {
+        red = 0;
+        green = 0;
+        blue = 255;
+    } else if (Celsius <= 50) {
+        red = (255 * Celsius / 50);
+        green = (255 * Celsius / 50);
+        blue = 255 - (255 * Celsius / 50);
+    } else if (Celsius <= 100) {
+        red = 255;
+        green = 255 - (255 * Celsius / 100);
+        blue = 0;
+    } else {
+        red = 255;
+        green = 0;
+        blue = 0;
+    }
+    redHex = parseInt(red)
+    var r = redHex.toString(16)
+    if (r.length < 2) {
+        r = "0" + r
+    }
+    greenHex = parseInt(green)
+    var g = greenHex.toString(16)
+    if (g.length < 2) {
+        g = "0" + g
+    }
+    blueHex = parseInt(blue)
+    var b = blueHex.toString(16)
+    if (b.length < 2) {
+        b = "0" + b
+    }
+
+
+    var rgb = "#" + r + g + b
+    var body = document.querySelector("body")
+    var corFundo = body.style.backgroundColor = rgb
 }
 
 function funcaoFahrenheit() {
     var inputFahrenheit = document.querySelector('#Fahrenheit')
     var Fahrenheit = parseFloat(inputFahrenheit.value)
-    var Kelvin = (75 * Fahrenheit + 459.67) * 5 / 9
+    var Kelvin = (Fahrenheit - 32) * 5 / 9 + 273.15
     var Celsius = (Fahrenheit - 32) / 1.8
 
     var outputCelsius = document.querySelector("#r_celsius")
-    outputCelsius.textContent = Celsius
+    outputCelsius.textContent = parseInt(Celsius) + " ºC"
     var outputKelvin = document.querySelector("#r_kelvin")
-    outputKelvin.textContent = Kelvin
+    outputKelvin.textContent = parseInt(Kelvin) + " K"
     var outputFahrenheit = document.querySelector("#r_fahrenheit")
-    outputFahrenheit.textContent = Fahrenheit
+    outputFahrenheit.textContent = parseInt(Fahrenheit) + " ºF"
+    var red = ''
+    var green = ''
+    var blue = ''
+    if (Celsius <= 0) {
+        red = 0;
+        green = 0;
+        blue = 255;
+    } else if (Celsius <= 50) {
+        red = (255 * Celsius / 50);
+        green = (255 * Celsius / 50);
+        blue = 255 - (255 * Celsius / 50);
+    } else if (Celsius <= 100) {
+        red = 255;
+        green = 255 - (255 * Celsius / 100);
+        blue = 0;
+    } else {
+        red = 255;
+        green = 0;
+        blue = 0;
+    }
+    redHex = parseInt(red)
+    var r = redHex.toString(16)
+    if (r.length < 2) {
+        r = "0" + r
+    }
+    greenHex = parseInt(green)
+    var g = greenHex.toString(16)
+    if (g.length < 2) {
+        g = "0" + g
+    }
+    blueHex = parseInt(blue)
+    var b = blueHex.toString(16)
+    if (b.length < 2) {
+        b = "0" + b
+    }
+
+
+    var rgb = "#" + r + g + b
+    var body = document.querySelector("body")
+    var corFundo = body.style.backgroundColor = rgb
 }
